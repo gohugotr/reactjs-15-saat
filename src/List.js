89ea210f83b37b3698916d8281347ceb6f1a72e9
Cengiz YILMAZ <terabilgi@gmail.com>
2022-04-05 12:25:05 +0300

    Arama fonksiyonu için ek açıklama yapıldı

--- src/List.js
+++ src/List.js
@@ -3,18 +3,50 @@ import "./List.css";
 import PropTypes from "prop-types";
 
 class List extends Component {
+
+  state = 
+  {
+    filterText:'',
+  }
+
+  OnChangeFilterText = (e) => 
+  {
+    //console.log(e.target.value);
+    this.setState({
+      filterText: e.target.value,
+    });
+  };
+
   render() {
+
+    const filteredContacts = this.props.contacts.filter(
+        contact => {
+          // name alanı için harf girildiğinde, indexOf "-1" değilse 
+          //(harf eşleşiyorsa)  sonuç ver.
+            return contact.name.toLowerCase().indexOf(
+                this.state.filterText.toLowerCase()
+            ) !==-1;
+          // 'cengiz'.indexOf('n'); ==> 'n' harfi kaçıncı harf. Yoksa -1 döner.
+        }
+    )
+
     return (
       <div className={"listArea"}>
-        <input type="text" name="filter" id="filter" placeholder="Filter by name or phone" />
+        <input type="text" value={this.state.filterText} onChange={this.OnChangeFilterText}
+         name="filter" id="filter" placeholder="Filter by name or phone" />
+
         <ul className={"list"}>
-          {this.props.contacts.map((kisiler) => (
-            <li key={kisiler.phone}>
-              <span className="name">{kisiler.name}</span>
-              <span className="phone">{kisiler.phone}</span>
-            </li>
-          ))}
+          {
+            filteredContacts.map((kisiler) => (
+           // this.props.contacts.map((kisiler) => (
+              <li key={kisiler.phone}>
+                <span className="name">{kisiler.name}</span>
+                <span className="phone">{kisiler.phone}</span>
+              </li>
+            ))
+          }
         </ul>
+ 
       </div>
     );
   }
