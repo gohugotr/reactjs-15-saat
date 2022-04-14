dca0edb14a78291278c133d6b1e3952a7d9bfb87
Cengiz YILMAZ <terabilgi@gmail.com>
2022-04-14 03:51:02 +0300

    Yeni nesil Route güncellendi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import React,{Component} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component{
+  
+  render()
+  {
+    const Anasayfa = () => {
+      return (
+          <h2>Anasayfa</h2>
+      );
+    };
+    
+    const İletisim = () => {
+      return (
+        <h3>İletişim</h3>
+      )
+    };
+    
+      return (
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Anasayfa />} />
+              <Route path="/iletisim" element={<İletisim />} />
+            </Routes>
+          </div>
+        </Router>
+      );
+  }
 }
 
 export default App;
