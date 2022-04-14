0d6ce30c4c89c4b3b27fa9e8b15392e68e3d6c97
Cengiz YILMAZ <terabilgi@gmail.com>
2022-04-14 15:30:53 +0300

    Route kullanımı

--- README.md
+++ README.md
@@ -4,4 +4,47 @@
 
 `npm i react react-dom react-router-dom`
 
+### import edilme şekli
 
+<code>import { BrowserRouter as Router, Routes, Route } from "react-router-dom";</code>
+
+- App.js içindeki ana kapsayıcı `BrowserRouter` olmalıdır.
+- `BrowserRouter as Router` şeklinde de kısaltılabilir.
+- `Router` kapsayıcısının içine `<Routes> ... </Routes>` tanımlanır.
+- `<Routes> ... </Routes>` içinde ihtiyaç duyulduğu kadar `Route` tanımlanabilir.
+
+### Route kullanım şekli
+
+```js script
+    <Route path="/" element={<Anasayfa />} />
+    <Route path="iletisim" exact element={<Iletisim />} />
+    <Route path="destek" element={<h2>Destek</h2>} />
+```
+
+### Tam kullanım şekli
+
+```js script
+class App extends Component {
+  render() {
+    const Anasayfa = () => {
+      return <h2>Anasayfa</h2>;
+    };
+
+    const Iletisim = () => {
+      return <h3>İletişim</h3>;
+    };
+
+    return (
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" exact element={<Anasayfa />} />
+            <Route path="iletisim" element={<Iletisim />} />
+            <Route path="destek" element={<h2>Destek</h2>} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
+}
+```
--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import './App.css';
-import React,{Component} from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-class App extends Component{
-  
-  render()
-  {
+class App extends Component {
+  render() {
     const Anasayfa = () => {
-      return (
-          <h2>Anasayfa</h2>
-      );
+      return <h2>Anasayfa</h2>;
     };
-    
-    const İletisim = () => {
-      return (
-        <h3>İletişim</h3>
-      )
+
+    const Iletisim = () => {
+      return <h3>İletişim</h3>;
     };
-    
-      return (
-        <Router>
-          <div className="App">
-            <Routes>
-              <Route path="/" element={<Anasayfa />} />
-              <Route path="/iletisim" element={<İletisim />} />
-            </Routes>
-          </div>
-        </Router>
-      );
+
+    return (
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Anasayfa />} />
+            <Route path="iletisim" exact element={<Iletisim />} />
+            <Route path="/destek" exact element={<h2>Destek</h2>} />
+          </Routes>
+        </div>
+      </Router>
+    );
   }
 }
 
